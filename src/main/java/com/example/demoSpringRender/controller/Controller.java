ecff8efb6ef7bf25b728dc0f1c315d9ca5e8c547
AnhTu <3artemis98@gmail.com>
2023-10-15 21:26:37 +0700

    lead product

--- src/main/java/com/example/demoSpringRender/controller/Controller.java
+++ src/main/java/com/example/demoSpringRender/controller/Controller.java
@@ -19,7 +19,7 @@ public class Controller {
     @Autowired
     private ProductRepository productRepository;
 
-	@GetMapping("./")
+	@GetMapping("/")
 	public String index(Model model, Principal principal, @RequestParam(value = "searchTerm", required = false) String searchTerm) {
 		 List<Product> topProducts;
 	        List<Product> regularProducts;
@@ -51,7 +51,29 @@ public class Controller {
 	}
 	
 	@GetMapping("/leaders")
-	public String showLeader() {
+	public String showLeader(Model model, Principal principal, @RequestParam(value = "searchTerm", required = false) String searchTerm) {
+		 List<Product> topProducts;
+	        List<Product> regularProducts;
+
+	        if (searchTerm != null && !searchTerm.isEmpty()) {
+	            // Nếu có từ khóa tìm kiếm, thực hiện tìm kiếm sản phẩm
+	            topProducts = productRepository.findByProductTopAndNameContaining(true, searchTerm);
+	            regularProducts = productRepository.findByNameContaining(searchTerm);
+	        } else {
+	            // Nếu không có từ khóa tìm kiếm, hiển thị tất cả sản phẩm
+	            topProducts = productRepository.findByProductTop(true);
+	            regularProducts = productRepository.findAll();
+	        }
+
+	        model.addAttribute("topProducts", topProducts);
+	        model.addAttribute("regularProducts", regularProducts);
+
+	        if (principal != null) {
+	            model.addAttribute("usernamePrin", principal.getName());
+	        } else {
+	            model.addAttribute("usernamePrin", "");
+	        }
+
 		return "leaders";
 	}
 	
